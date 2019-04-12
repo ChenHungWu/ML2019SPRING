f45b8164c1fb37579c2892bd1aa7c17d63540a85
WuChenHung <ac791228@gmail.com>
2019-04-12 12:58:11 +0800

    upload

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -35,14 +35,14 @@ if(len(sys.argv)>3):
     text.close()    
     x = []
     y = []
-    # 每 12 個月
+    # 12 months
     for i in range(12):    
-    # 一個月取連續10小時的data可以有471筆
+    # 471 continus 10 hours data in a month 
         for j in range(471):
             x.append([])
-            # 18種污染物
+            # 18 contaminant
             for t in range(18):
-                # 連續9小時
+                # 9 hours
                 for s in range(9):
                     for m in range(1,degree+1):
                         if((data[t][480*i+j+s])<0):
@@ -52,7 +52,7 @@ if(len(sys.argv)>3):
             y.append(data[9][480*i+j+9])
     x = np.array(x)
     y = np.array(y)    
-    #把小於0的data換成平均
+    #change the data which small than zero into average
     for i in range(len(x)):
         for j in range(0,len(x[i]),9*degree):
             for k in range(9*degree):
@@ -62,76 +62,47 @@ if(len(sys.argv)>3):
                     else:
                         x[i][j+k] = np.average( [ x[i-1][j:j+degree*9:degree] ] )
 
-
-    #Normalization 可是效果不好
-    '''
-    mean = np.mean(x, axis = 0) 
-    std = np.std(x, axis = 0)
-    for i in range(x.shape[0]):
-        for j in range(x.shape[1]):
-            if not std[j] == 0 :
-                x[i][j] = (x[i][j]- mean[j]) / std[j]
-    '''
-    '''
-    reverse_column = []
-    for i in range(18):
-    	reverse_column.append(i*9+8)
-    	reverse_column.append(i*9+7)
-    	reverse_column.append(i*9+6)
-    	reverse_column.append(i*9+5)
-    	reverse_column.append(i*9+4)
-    reverse_column = range(81,90)
-    x = x[:,reverse_column]
-    '''
-    # add bias 在所有data前面加排1代表bias
-
-    
+    # add bias
     x = np.concatenate((np.ones((x.shape[0],1)),x), axis=1)
+
     w = np.zeros(len(x[0]))
     mt = np.zeros(len(x[0]))
     vt = np.zeros(len(x[0]))
-   
-    
-    #Adam 40萬 0.0001
+
+    #Adam 400000 0.0001
     l_rate = 0.0001 
     repeat = 400000
     b1 = 0.9
     b2 = 0.999
     e = 1e-8
     x_t = x.transpose()
-    #lamda = [5,3,1,0.1,0.01,0.001,0.0001]
-    #color = ['red','green','blue','gray','peru','g','gold']
-    
     
     for i in range(1,repeat):
         #calculate gradient
         diff = np.dot(x,w)
         loss = diff - y
-        #regularization
-        #loss += lamda[t] * np.sum((np.concatenate((np.array([0]),w[1:]),axis=0) **2 ))
-        gra = 2.0 * np.dot(x_t,loss) #+ 2*lamda[t]*(np.concatenate((np.array([0]),w[1:]),axis=0))
-        #update weight
+        gra = 2.0 * np.dot(x_t,loss)
         mt = b1*mt + (1-b1)*gra
         vt = b2*vt + (1-b2)*(gra*gra)
         m_cap = mt / (1-(b1**i))
         v_cap = vt / (1-(b2**i))
         w = w - (l_rate * m_cap) / (np.sqrt(v_cap)+e) 
+
         #calculate cost
         cost = np.sum(loss**2) / len(x)
         cost_a  = math.sqrt(cost)
         print ('iteration: %d | Cost: %.9f  ' % ( i,cost_a))
-   # np.save('my_w.npy',w)
-   # np.save('my_mt.npy',mt)
-   # np.save('my_vt.npy',vt)
-
+   #np.save('w_mt_vt.npy', [w, mt, vt])
 
 
 if(len(sys.argv)==3):
-    w = np.load('my_w.npy')
-    mt = np.load('my_mt.npy')
-    vt = np.load('my_vt.npy')
+    w_mt_vt = np.load('w_mt_vt.npy')
+    w = w_mt_vt[0]
+    mt = w_mt_vt[1]
+    vt = w_mt_vt[2]
 # read model
 #w = np.load('model.npy')
+
 #input test data
 test_x = []
 n_row = 0
@@ -155,8 +126,7 @@ for r in row:
 text.close()
 test_x = np.array(test_x)
 
-#把小於0的data換成平均
-for i in range(len(test_x)):
+#change the data which small than zero into average
     for j in range(0,len(test_x[i]),degree*9):
         for k in range(degree*9):
             if(test_x[i][j+k]<0):
@@ -165,14 +135,6 @@ for i in range(len(test_x)):
                 else :
                     test_x[i][j+k] = np.average( [ test_x[i-1][j:j+degree*9:degree] ] )
 
-#Normalization 可是效果不好
-'''
-for i in range(test_x.shape[0]):
-    for j in range(test_x.shape[1]):
-        if not std[j] == 0 :
-            test_x[i][j] = (test_x[i][j]- mean[j]) / std[j]
-'''
-#test_x = test_x[:,reverse_column]
 # add bias
 test_x = np.concatenate((np.ones((test_x.shape[0],1)),test_x), axis=1)
 
@@ -182,6 +144,7 @@ for i in range(len(test_x)):
     a = np.dot(w,test_x[i])
     ans[i].append(a)
 
+
 filename = result_name
 text = open(filename, "w+")
 s = csv.writer(text,delimiter=',',lineterminator='\n')
@@ -189,4 +152,3 @@ s.writerow(["id","value"])
 for i in range(len(ans)):
     s.writerow(ans[i]) 
 text.close()
-
