d04705f6ae2156b710a6897dcc08927ad6f240d8
WuChenHung <ac791228@gmail.com>
2019-05-10 13:24:29 +0800

    hw6

--- hw6/hw6_test.sh
+++ hw6/hw6_test.sh
@@ -1,4 +1,5 @@
 # $1 test_x.csv dir
 # $2 dict.txt.big dir
 # $3 output file dir
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/word2vec_250.model
 python3 test_model.py $1 $2 $3
--- hw6/hw6_train.sh
+++ hw6/hw6_train.sh
@@ -2,4 +2,13 @@
 # $2 train_y.csv dir
 # $3 test_x.csv dir
 # $4 dict.txt.big dir
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/word2vec_250.model
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_00.json
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_00.weight
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_01.json
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_01.weight
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_04.json
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_04.weight
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_07.json
+wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_07.weight
 python3 train_model.py $1 $2 $3 $4
