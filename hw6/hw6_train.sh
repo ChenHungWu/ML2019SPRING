5446afbcf7d229d5a63fd221fc530738bb8c7d14
WuChenHung <ac791228@gmail.com>
2019-05-10 13:46:28 +0800

    hw6

--- hw6/hw6_train.sh
+++ hw6/hw6_train.sh
@@ -5,12 +5,4 @@
 wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/word2vec_250.model
 wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/word2vec_250.model.trainables.syn1neg.npy
 wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/word2vec_250.model.wv.vectors.npy
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_00.json
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_00.weight
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_01.json
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_01.weight
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_04.json
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_04.weight
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_07.json
-wget https://github.com/WuChenHung/ML2019SPRING/releases/download/hw6/rnn250_07.weight
 python3 train_model.py $1 $2 $3 $4
